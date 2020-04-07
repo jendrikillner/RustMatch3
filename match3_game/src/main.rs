649b8e88d7d86f215fb411f03648741428735859
Jendrik Illner <jendrik.illner@gmail.com>
2020-04-06 20:43:54 -0400

    * moved time extension functions into a separate module

--- match3_game/src/main.rs
+++ match3_game/src/main.rs
@@ -14,9 +14,7 @@ use winapi::um::d3dcommon::*;
 use winapi::um::winuser::*;
 use winapi::Interface;
 
-fn as_fractional_secs(dur: &std::time::Duration) -> f32 {
-    (dur.as_secs() as f64 + f64::from(dur.subsec_nanos()) / 1_000_000_000.0) as f32
-}
+use std_time_ext::*;
 
 struct WindowCreatedData {
     hwnd: HWND,
---
+++ std_time_ext/src/std_time_ext.rs
@@ -0,0 +1,3 @@
+pub fn as_fractional_secs(dur: &std::time::Duration) -> f32 {
+    (dur.as_secs() as f64 + f64::from(dur.subsec_nanos()) / 1_000_000_000.0) as f32
+}
