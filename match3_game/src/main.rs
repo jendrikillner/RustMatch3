6f71b0e5cc58e917df08a27c8e1bd3f53150dfbb
Jendrik Illner <jendrik.illner@gmail.com>
2020-04-13 16:12:46 -0400

    * removed double use

--- match3_game/src/main.rs
+++ match3_game/src/main.rs
@@ -1,6 +1,5 @@
 use graphics_device::*;
 use os_window::*;
-use graphics_device::*;
 
 pub fn as_fractional_secs(dur: &std::time::Duration) -> f32 {
     (dur.as_secs() as f64 + f64::from(dur.subsec_nanos()) / 1_000_000_000.0) as f32
