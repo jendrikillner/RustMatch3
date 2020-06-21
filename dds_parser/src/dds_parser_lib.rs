411d246ce5689d747e1a7eff2aa14d912d0d9284
Jendrik Illner <jendrik.illner@gmail.com>
2020-06-21 14:04:17 -0400

    * added first dummy interface for the DDS parser

---
+++ dds_parser/src/dds_parser_lib.rs
@@ -0,0 +1,46 @@
+use winapi::um::d3d11::*;
+use winapi::shared::dxgiformat::*;
+use winapi::shared::dxgitype::*;
+
+pub enum DdsParserError {
+	InvalidHeader
+}
+
+pub fn parse_dds_header( src_data : &[u8] ) -> Result<D3D11_TEXTURE2D_DESC, DdsParserError> {
+	Err(DdsParserError::InvalidHeader)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+	// embed the data we will be testing against
+	mod paintnet {
+		pub static BLACK_4X4_BC1      : &'static [u8; 240128] = include_bytes!("../tests/data/paintnet/black_4x4_bc1.dds");
+		// pub static BLACK_4X4_MIPS_BC1 : &'static [u8; 320552] = include_bytes!("../tests/data/paintnet/black_4x4_mips_bc1.dds");
+	}
+
+    #[test]
+    fn sum_test() {
+
+		let texture_header_ref = D3D11_TEXTURE2D_DESC {
+			Width : 4,
+			Height : 4,
+			MipLevels : 0,
+			ArraySize : 0,
+			Format : DXGI_FORMAT_BC1_UNORM,
+			SampleDesc : DXGI_SAMPLE_DESC  {
+				Count : 1,
+				Quality : 1,
+			},
+			Usage : D3D11_USAGE_DEFAULT,
+			BindFlags : 0,
+			MiscFlags : 0,
+			CPUAccessFlags : 0,
+		};
+
+        let texture_load_result = parse_dds_header( paintnet::BLACK_4X4_BC1 );
+
+		assert_eq!( texture_load_result.is_ok(), true );
+    }
+}
