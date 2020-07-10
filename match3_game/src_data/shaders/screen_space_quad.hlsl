1307f3ac6846b20997bedbb2931064259b1508b8
Jendrik Illner <jendrik.illner@gmail.com>
2020-07-09 22:50:05 -0400

    * added texture support to the shader

--- match3_game/src_data/shaders/screen_space_quad.hlsl
+++ match3_game/src_data/shaders/screen_space_quad.hlsl
@@ -8,11 +8,15 @@ cbuffer ScreenSpaceQuadData : register(b0)
 struct VertexToPixelShader
 {
 	float4 position_clip : SV_POSITION;
+	float2 uv : TEXCOORD0;
 };
+
+Texture2D Texture;
+SamplerState Sampler;
 
 VertexToPixelShader VS_main(uint vertex_id: SV_VertexID)
 {
-    VertexToPixelShader output;
+	VertexToPixelShader output = (VertexToPixelShader) 0;
 
     switch (vertex_id) {
     case 0: output.position_clip = float4(-1,  1, 0, 1); break; // top-left
@@ -20,6 +24,13 @@ VertexToPixelShader VS_main(uint vertex_id: SV_VertexID)
     case 2: output.position_clip = float4(-1, -1, 0, 1); break; // bottom-left
     case 3: output.position_clip = float4( 1, -1, 0, 1); break; // bottom-right
     }
+
+	switch (vertex_id) {
+	case 0: output.uv = float2(0, 0 ); break; // top-left
+	case 1: output.uv = float2(1, 0 ); break; // top-right
+	case 2: output.uv = float2(0, 1 ); break; // bottom-left
+	case 3: output.uv = float2(1, 1 ); break; // bottom-right
+	}
 
 	output.position_clip.xy *= scale;
 	output.position_clip.xy += position;
@@ -29,5 +40,5 @@ VertexToPixelShader VS_main(uint vertex_id: SV_VertexID)
 
 float4 PS_main(VertexToPixelShader input) : SV_TARGET
 {
-	return color;
+	return color * Texture.Sample(Sampler, input.uv);
 }
