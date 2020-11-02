5679632885f6192ec61fef758e8ab6be7c22659a
Jendrik Illner <jendrik.illner@gmail.com>
2020-11-02 00:35:54 -0500

    * moved shader compilation into ninja * introduced main ninja file that compiles all data

--- match3_game/compile_src_data.bat
+++
@@ -1,6 +0,0 @@
-:: compile VS shader
-:: ..\build_environment\microsoft\fxc\fxc.exe /T vs_4_0 src_data\shaders\screen_space_quad.hlsl /EVS_main /Fo target_data\shaders\screen_space_quad.vsb
-:: ..\build_environment\microsoft\fxc\fxc.exe /T ps_4_0 src_data\shaders\screen_space_quad.hlsl /EPS_main /Fo target_data\shaders\screen_space_quad.psb
-
-..\build_environment\microsoft\fxc\fxc.exe /T vs_4_0 src_data\shaders\game_space_quad.hlsl /EVS_main /Fo target_data\shaders\game_space_quad.vsb
-..\build_environment\microsoft\fxc\fxc.exe /T ps_4_0 src_data\shaders\game_space_quad.hlsl /EPS_main /Fo target_data\shaders\game_space_quad.psb
